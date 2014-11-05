479d14306128619708e89081cb1c3b6aaeddf253
GunnarSturla <gunnar@gunnarsturla.com>
2014-11-05 19:46:19 +0000

    Núna er alltaf sami action bar

--- app/src/main/java/com/gunnarsturla/restaurantappgi/SubMenuActivity.java
+++ app/src/main/java/com/gunnarsturla/restaurantappgi/SubMenuActivity.java
@@ -54,7 +54,7 @@ public class SubMenuActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_sub_menu, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
