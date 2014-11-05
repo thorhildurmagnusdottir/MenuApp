86b9e9a71c223c4fa1546feefad11b4ada2b1448
Thorleifur <tholli@heimsnet.is>
2014-11-05 13:48:50 +0000

    Ordered_Single_Row_Adapter

---
+++ app/src/main/java/com/gunnarsturla/restaurantappgi/Orderd.java
@@ -0,0 +1,39 @@
+package com.gunnarsturla.restaurantappgi;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.Vector;
+
+/**
+ * Created by Thorleifur on 05/11/14.
+ */
+public class Orderd extends BaseAdapter {
+
+    ListView ordered;
+
+    String [] Items; // Hér vantar að kalla á heiti réttar
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
