e2e0f5074683ebefc0c18f638ae6a85a75943305
GunnarSturla <gunnar@gunnarsturla.com>
2014-11-05 21:32:59 +0000

    Tiltekt

--- app/src/main/java/com/gunnarsturla/restaurantappgi/main.java
+++ app/src/main/java/com/gunnarsturla/restaurantappgi/main.java
@@ -9,9 +9,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 /**
  * @author Gunnar Sturla Ágústuson
