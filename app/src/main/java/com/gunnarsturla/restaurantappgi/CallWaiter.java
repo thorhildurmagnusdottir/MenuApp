6439f003396021b524141d50d6eb5fe310c2831d
GunnarSturla <gunnar@gunnarsturla.com>
2014-11-19 16:44:44 +0000

    CallWaiter komið inn

---
+++ app/src/main/java/com/gunnarsturla/restaurantappgi/CallWaiter.java
@@ -0,0 +1,31 @@
+package com.gunnarsturla.restaurantappgi;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+/**
+ * Created by Dagny on 19.11.2014.
+ */
+
+public class CallWaiter  {
+
+    public static void callme(final Activity activity) {
+        AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
+        alertDialog.setMessage("Vantar þig aðstoð frá þjóni?");
+        alertDialog.setButton("Já", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(activity.getApplicationContext(), "Þjónn kemur skjótt", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertDialog.setButton2("Nei", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        alertDialog.setIcon(R.drawable.ic_launcher);
+        alertDialog.show();
+    }
+}
