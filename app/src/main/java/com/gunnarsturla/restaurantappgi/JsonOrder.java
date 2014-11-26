4bf4f5687d1f6a6c46a92e433f3a571eedf7b96f
daggala <daggala@gmail.com>
2014-11-26 12:21:24 +0000

    JsonOrder
    Setur pöntun inn í json

---
+++ app/src/main/java/com/gunnarsturla/restaurantappgi/JsonOrder.java
@@ -0,0 +1,43 @@
+package com.gunnarsturla.restaurantappgi;
+
+import menu.Order;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Dagny on 24.11.2014.
+ */
+public class JsonOrder{
+
+    public static String toJSon() {
+
+        try {
+
+            JSONObject jsonObj = new JSONObject();
+
+            JSONArray jsonArr = new JSONArray();
+
+            for(int i = 0; i < Order.size(); i++) {
+                JSONObject order = new JSONObject();
+                order.put("order", Order.get(i).getName());
+                jsonArr.put(order);
+            }
+
+            jsonObj.put("orderlist", jsonArr);
+
+            return jsonObj.toString();
+
+        }
+
+        catch(JSONException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+
+
+
+    }
+
+}
